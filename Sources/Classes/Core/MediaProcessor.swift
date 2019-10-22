01c7d4c2deb8b5737e35332c08e44a7e955d8d94
CATHERINE <catherine.kg@cabotsolutions.com>
2019-10-22 14:29:20 +0530

    process elements file name added

--- Sources/Classes/Core/MediaProcessor.swift
+++ Sources/Classes/Core/MediaProcessor.swift
@@ -14,7 +14,7 @@ public class MediaProcessor {
     public init() {}
     
     // MARK: - process elements
-    public func processElements(item: MediaItem, fileName: String, folderNamer: String, completion: @escaping ProcessCompletionHandler) {
-        item.type == .video ? processVideoWithElements(item: item, completion: completion) : processImageWithElements(item: item, completion: completion)
+    public func processElements(item: MediaItem, fileName: String, folderName: String, completion: @escaping ProcessCompletionHandler) {
+        item.type == .video ? processVideoWithElements(item: item,videoFileName:fileName, folderName: folderName, completion: completion) : processImageWithElements(item: item, completion: completion)
     }
 }
